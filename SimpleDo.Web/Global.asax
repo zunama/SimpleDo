e79d151704523a01e02be2ec73ffdb9ca8a20994
James <james@zunama.com>
2014-06-29 11:23:58 -0700

    Init project setup

---
+++ SimpleDo.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SimpleDo.Web.MvcApplication" Language="C#" %>
---
+++ SimpleDo.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SimpleDo.Web.Startup))]
+namespace SimpleDo.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
